4b4a544d1d5e52f5c409282bdd53abe43d557b79
chandrashekhar235 <sahiljoshi399@gmail.com>
2026-03-15 02:09:01 +0530

    fix blog user ObjectId schema

--- backend/models/Blog.js
+++ backend/models/Blog.js
@@ -1,38 +1,44 @@
-const mongoose = require("mongoose"); // called moongoose tool
+const mongoose = require("mongoose");
 
-const BlogSchema = new mongoose.Schema( // defined a structure tht how the data should be saved
-  {
-    title: {
-      type: String,
-      required: true,
-    },
-    description: {
-      type: String,
-      required: true,
-    },
-    author: {
-      type: String,
-      required: true,
-    },
-    user: {
-      type: mongoose.Schema.types.ObjectId,
-      ref: "User",
-      required: true,
-    },
-    date: {
-      type: Date,
-      default: Date.now,
-    },
-    source: {
-      type: [String],
-      default: [],
-    },
-    references: {
-      type: [String],
-      default: [],
-    },
+const BlogSchema = new mongoose.Schema(
+{
+  title: {
+    type: String,
+    required: true,
   },
-  { timestamps: true }
-);
-module.exports = mongoose.model("Blog", BlogSchema); // created a object blog tht will use all this 
 
+  description: {
+    type: String,
+    required: true,
+  },
+
+  author: {
+    type: String,
+    required: true,
+  },
+
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+
+  source: {
+    type: [String],
+    default: [],
+  },
+
+  references: {
+    type: [String],
+    default: [],
+  },
+},
+{ timestamps: true }
+);
+
+module.exports = mongoose.model("Blog", BlogSchema);
